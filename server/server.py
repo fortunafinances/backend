0acbe7bd5b9aa31ef2c613049bed1cf338dd8e71
John ryan Byers <johbyers@publicisgroupe.net>
2023-07-11 15:56:28 -0400

    cleaned up server file

--- server/server.py
+++ server/server.py
@@ -4,7 +4,6 @@ from ariadne.explorer import ExplorerGraphiQL
 from flask_cors import CORS, cross_origin
 from model import query, mutation
 
-
 """
     This is the server file which handles the GraphQL route. The route we
     will be using is /graphql
@@ -17,11 +16,7 @@ EXPLORER_HTML = ExplorerGraphiQL().html(None)
 
 type_defs = gql(load_schema_from_path("schema.graphql"))
 schema = make_executable_schema(type_defs, query, mutation)
-
 app = Flask(__name__)
-#CORS(app, origins=["http://localhost:5000"])
-
-#app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/')
@@ -39,7 +34,7 @@ def graphql_playground():
 @app.route("/graphql", methods=["POST", "OPTIONS"])
 @cross_origin()
 def graphql_server():
-    if request.method == "OPTIONS":
+    if request.method == "OPTIONS":   # CORS sends an options request from the frontend before a POST
         return _build_cors_preflight_response()
     elif request.method == "POST":
         data = request.get_json()
@@ -52,20 +47,21 @@ def graphql_server():
         )
 
         response = jsonify(result)
-        #response.headers.add("Access-Control-Allow-Origin", "*")
         status_code = 200 if success else 400
         return response
     else:
         raise RuntimeError("Can not handle method {}".format(request.method))
 
-#"""
+"""
+This is a helper method for the POST requests. It gives access control to the browser
+to solve access for CORS.
+"""
 def _build_cors_preflight_response():
     response = make_response()
     response.headers.add("Access-Control-Allow-Origin", "*")
     response.headers.add('Access-Control-Allow-Headers', "*")
     response.headers.add('Access-Control-Allow-Methods', "*")
     return response
-#"""
 
 if __name__ == '__main__':
     app.run(debug=True)  # debug=True allows the server to restart itself
