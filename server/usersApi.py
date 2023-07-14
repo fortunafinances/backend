0e7cf8c66ebe3db8f8a843af1477b76640a90fb4
LV <linvu2@publicisgroupe.net>
2023-07-14 10:20:48 -0400

    deleted dup auth code and edited usersApi

--- server/usersApi.py
+++ server/usersApi.py
@@ -4,24 +4,24 @@ from functools import wraps
 import sys
 from flask_cors import CORS
 
-
 api_blueprint = Blueprint('api', __name__)
 
 def authenticate(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         auth_header = request.headers.get('Authorization', None)
-        print("SOMETHING.... ", auth_header, " ", file=sys.stdout)
+        print("AUTH HEADER.... ", auth_header, " ", file=sys.stdout)
         if not auth_header:
             return {'message': 'Authorization header is missinggg'}, 401
 
         token = auth_header.split(' ')[1]
-        print("TOKEN.... ", token, " ", file=sys.stdout)
+        print("\nTOKEN.... ", token, " ", file=sys.stdout)
         # TODO: validate the token using Auth0's API
         # if the token is valid, extract the user information and pass it to the wrapped function
         
         
         try:
+          print("\nSTARTING DECODING ACCESS TOKEN...", file=sys.stdout)
           # decode the token using the secret key used to sign it
           decoded_token = jwt.decode(token, 'my_secret_key', algorithms=['HS256'])
           # extract the user information from the decoded token
