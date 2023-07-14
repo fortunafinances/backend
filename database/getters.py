5d417ac311def977025e0dd0610e4f57c63c8a3c
Leila Baniassad <lbaniassad@gmail.com>
2023-07-14 10:43:22 -0400

    added getActivity(accID) + 2 helper getters

--- database/getters.py
+++ database/getters.py
@@ -20,3 +20,22 @@ def getStocks():
 def getAccs():
     accs = (Acc.query.all())
     return [acc.serialize() for acc in accs]
+
+#Returns a list of transfers and trades associated with a certain acc_id
+def getActivity(accID):
+    trades = getTrades(accID)
+    transfers = getTransfers(accID)
+    activity = trades + transfers
+    return activity
+
+#helper method that returns a list of all trades of a certain account id
+def getTrades(accID):
+    trades = (Trade.query.filter_by(acc_id = accID).all())
+    list_of_trades = [trade.serialize() for trade in trades]
+    return list_of_trades
+
+#helper method that returns a list of all the transfers associated with a certain account id
+def getTransfers(accID):
+    transfers = (Transfer.query.filter_by(acc_id = accID).all())
+    list_of_transfers = [transfer.serialize() for transfer in transfers]
+    return list_of_transfers
