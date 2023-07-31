ff25fa90e9119400260f1c00573dd4771711bade
Buzz Zhang <buzzhang@publicisgroupe.net>
2023-07-31 11:07:33 -0400

    Added accWatch functionality and existing users into the mockDb

--- mockData/mockDb.py
+++ mockData/mockDb.py
@@ -12,11 +12,10 @@ def initUsers():
     addUser("AUTHuser1", "tyj", "Tyler", "Jones", "tyjones@email.com", "4017324554", "picture", "Fortuna", True)
     addUser("AUTHuser2", "jr1", "JR", "Byers", "jr@email.com", "6174357213", "picture", "Fortuna", True)
     addUser("AUTHuser3", "claudia27", "Claudia", "Alves", "claudia_alves@email.com", "6177320019", "picture", "Fortuna", True)
+    addUser("auth0|64bff46269ace00bf6a23c03", "lindsay.rl", "Lindsay", "Liu", "rll46@duke.edu", "8646247257", "picture", "Fortuna", True)
+    addUser("auth0|64a5a9b2bb372f7bd87fc5e5", "linhvu", "Linh", "Vu", "linh.vu@publicissapient.com", "0000000000", "picture", "Fortuna", True)
+    addUser("auth0|64b7f9420f43178a8a19c036", "nrgbistro", "Nolan", "Gelinas", "nolangelinas@gmail.com", "1610308884", "picture", "Fortuna", True)
 
-    # This needs to be done twice because the first time a user is added their onboarding is set to false
-    addUser("AUTHuser1", "tyj", "Tyler", "Jones", "tyjones@email.com", "4017324554", "picture", "Fortuna", True)
-    addUser("AUTHuser2", "jr1", "JR", "Byers", "jr@email.com", "6174357213", "picture", "Fortuna", True)
-    addUser("AUTHuser3", "claudia27", "Claudia", "Alves", "claudia_alves@email.com", "6177320019", "picture", "Fortuna", True)
 
 def initAccs():
     addAcc("Brokerage account", "AUTHuser1", 10000.00)
@@ -25,6 +24,11 @@ def initAccs():
 
     addAcc("Brokerage account", "AUTHuser2", 3000.00)
     addAcc("Brokerage account", "AUTHuser3", 7000.00)
+
+    addAcc("College Fund", "auth0|64bff46269ace00bf6a23c03", 100000.00)
+    addAcc("Brokerage account", "auth0|64a5a9b2bb372f7bd87fc5e5", 100000.00)
+    addAcc("Brokerage account Fund", "auth0|64b7f9420f43178a8a19c036", 100000.00)
+    
 
 def initAccsHistory():
     accs = getAccs()
@@ -40,8 +44,23 @@ def initAccHistory(accId):
     
     for factor, sp500Log in enumerate(sp500Data, 52):
         value = acc.cash + (acc.cash * factor * 0.0005) + (sp500Log.price * random.randint(-5, 10) / 100.0) 
-        print(sp500Log.date, acc.cash, value, sp500Log.price, (value / acc.cash))
+        # print(sp500Log.date, acc.cash, value, sp500Log.price, (value / acc.cash))
         addAccHistory(accId, value, sp500Log.date)
+
+def initAccWatch():
+    addAccWatch(6, "META")
+    addAccWatch(6, "JPM")
+
+    addAccWatch(7, "DNUT")
+    addAccWatch(7, "V")
+    addAccWatch(7, "HD")
+
+    addAccWatch(8, "EXC")
+    addAccWatch(8, "INTU")
+    addAccWatch(8, "MAR")
+    addAccWatch(8, "MRK")
+    addAccWatch(8, "MTB")
+    addAccWatch(8, "VZ")
 
 def initBuyMarket():
     buyMarket(1, "V", 5)
